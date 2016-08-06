73e79b1980b88ab23d50517f70a707e106a4379e
Ryan Globus <rglobus@apple.com>
2016-08-05 18:34:08 -0700

    Refactored

--- spoon.js
+++ spoon.js
@@ -1,182 +1,190 @@
-window.onload = function() {
+var SPOON_APP = SPOON_APP || {};
 
-    var game, player, cursors, bullets, fireButton, forks, score, scoreText, lives, livesText, starfield, enemyLives, enemyText;
-    var SCORE_PREFIX = 'Score: ';
-    var LIVES_PREFIX = 'Lives: ';
-    var ENEMY_PREFIX = 'Enemy: ';
-    var gameDiv = document.getElementById('game');
+SPOON_APP.SCORE_PREFIX = 'Score: ';
+SPOON_APP.LIVES_PREFIX = 'Lives: ';
+SPOON_APP.ENEMY_PREFIX = 'Enemy: ';
 
-    function preload() {
-        game.load.image('star', 'assets/star.png');
-        game.load.image('bullet', 'assets/bullet.png');
-        game.load.image('spoon', 'assets/spoon.png');
-        game.load.image('fork', 'assets/fork.png');
-        game.load.image('starfield', 'assets/starfield.png');
-    }
+SPOON_APP.preload = function() {
+    this.game.load.image('star', 'assets/star.png');
+    this.game.load.image('bullet', 'assets/bullet.png');
+    this.game.load.image('spoon', 'assets/spoon.png');
+    this.game.load.image('fork', 'assets/fork.png');
+    this.game.load.image('starfield', 'assets/starfield.png');
+}
 
-    function createPlayer() {
-        player = game.add.sprite((game.world.width - 48) / 2, game.world.height - 150, 'spoon');
-        game.physics.arcade.enable(player);
-        player.body.bounce.x = 0.5;
-        player.body.bounce.y = 0.5;
-        player.body.gravity.y = 0;
-        player.body.collideWorldBounds = true;
-        player.anchor = new Phaser.Point(0.5, 0.5);
-        player.body.drag = new Phaser.Point(100, 100); // TODO drag seems to be not at same angle as spoon
-    }
+SPOON_APP.createPlayer = function() {
+    this.player = this.game.add.sprite((this.game.world.width - 48) / 2, this.game.world.height - 150, 'spoon');
+    this.game.physics.arcade.enable(this.player);
+    this.player.body.bounce.x = 0.5;
+    this.player.body.bounce.y = 0.5;
+    this.player.body.gravity.y = 0;
+    this.player.body.collideWorldBounds = true;
+    this.player.anchor = new Phaser.Point(0.5, 0.5);
+    this.player.body.drag = new Phaser.Point(100, 100); // TODO drag seems to be not at same angle as spoon
+}
 
-    function createBullets() {
-        bullets = game.add.weapon(30, 'bullet');
-        bullets.enableBody = true;
-        bullets.physicsBodyType = Phaser.Physics.ARCADE;
-        bullets.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
-        bullets.bulletSpeed = 400;
-        bullets.fireRate = 200;
-        bullets.bulletAngleOffset = 90;
-        bullets.fireAngle = Phaser.ANGLE_UP;
-        bullets.trackSprite(player, 0, 0, true);
-        bullets.trackRotation = false;
-        bullets.onFire.add(function() {
-            score -= 1;
-            scoreText.text = SCORE_PREFIX + score;
-        });
-    }
+SPOON_APP.createBullets = function() {
+    this.bullets = this.game.add.weapon(30, 'bullet');
+    this.bullets.enableBody = true;
+    this.bullets.physicsBodyType = Phaser.Physics.ARCADE;
+    this.bullets.bulletKillType = Phaser.Weapon.KILL_WORLD_BOUNDS;
+    this.bullets.bulletSpeed = 400;
+    this.bullets.fireRate = 200;
+    this.bullets.bulletAngleOffset = 90;
+    this.bullets.fireAngle = Phaser.ANGLE_UP;
+    this.bullets.trackSprite(this.player, 0, 0, true);
+    this.bullets.trackRotation = false;
+    this.bullets.onFire.add(function() {
+        this.score -= 1;
+        this.scoreText.text = this.SCORE_PREFIX + this.score;
+    }, this);
+}
 
-    function createForks() {
-        forks = game.add.group();
-        forks.enableBody = true;
-        forks.physicsBodyType = Phaser.Physics.ARCADE;
-        for (var y = 0; y < 5; y++) {
-            for (var x = 0; x < 5; x++) {
-                var fork = forks.create(x * 100, y * -400 + 50, 'fork');
-                fork.angle = Math.random() * 360; // TODO func
-                fork.anchor = new Phaser.Point(0.5, 0.5);
-                // fork.body.collideWorldBounds = true;
-                fork.body.bounce.x = 1;
-                fork.body.bounce.y = 1;
-                fork.body.angularVelocity = 360;
-                // fork.body.velocity.x = Math.random() * 100 - 200;
-                fork.body.velocity.y = Math.random() * 50 + 25;
-                enemyLives++;
-            }
-        }
-        forks.x = 100;
-        forks.y = 50;
-    }
-
-    function createInput() {
-        cursors = game.input.keyboard.createCursorKeys();
-        fireButton = game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
-    }
-
-    function createTexts() {
-        var labelStyle = { font: '24px Arial', fill: '#fff' };
-        score = 0;
-        scoreText = game.add.text(10, 10, SCORE_PREFIX + score, labelStyle);
-
-        lives = 3;
-        livesText = game.add.text(game.world.width - 150, 10, LIVES_PREFIX, labelStyle);
-        for (var i = 0; i < lives; i++) {
-            livesText.text += "💛";
-        }
-
-        enemyText = game.add.text(game.world.width - 150, game.world.height - 40, ENEMY_PREFIX + enemyLives, labelStyle);
-    }
-
-    function create() {
-        enemyLives = 0;
-        starfield = game.add.tileSprite(0, 0, game.world.width, game.world.height, 'starfield');
-        game.physics.startSystem(Phaser.Physics.ARCADE);
-        createPlayer();
-        createBullets();
-        createForks();
-        createInput();
-        createTexts();
-    }
-
-    function bulletHitsEnemey(bullet, enemy) {
-        bullet.kill();
-        enemy.destroy();
-        enemyLives--;
-        enemyText.text = ENEMY_PREFIX + enemyLives;
-
-        score += 100;
-        scoreText.text = SCORE_PREFIX + score;
-    }
-
-    function playerHitsEnemy(player, enemy) {
-        enemy.destroy();
-        enemyLives--;
-        enemyText.text = ENEMY_PREFIX + enemyLives;
-        lives--;
-        score -= 200;
-        scoreText.text = SCORE_PREFIX + score;
-        if (lives > 0) {
-            // TODO "blink"
-            livesText.text = LIVES_PREFIX;
-            for (var i = 0; i < lives; i++) {
-                livesText.text += "💛";
-            }
-        } else {
-            // TODO
-            console.log('Game Over');
+SPOON_APP.createForks = function() {
+    this.forks = this.game.add.group();
+    this.forks.enableBody = true;
+    this.forks.physicsBodyType = Phaser.Physics.ARCADE;
+    for (var y = 0; y < 5; y++) {
+        for (var x = 0; x < 5; x++) {
+            var fork = this.forks.create(x * 100, y * -400 + 50, 'fork');
+            fork.angle = Math.random() * 360; // TODO func
+            fork.anchor = new Phaser.Point(0.5, 0.5);
+            // fork.body.collideWorldBounds = true;
+            fork.body.bounce.x = 1;
+            fork.body.bounce.y = 1;
+            fork.body.angularVelocity = 360;
+            // fork.body.velocity.x = Math.random() * 100 - 200;
+            fork.body.velocity.y = Math.random() * 50 + 25;
+            this.enemyLives++;
         }
     }
+    this.forks.x = 100;
+    this.forks.y = 50;
+}
 
-    function update() {
-        starfield.tilePosition.y += 2;
-        // TODO inertia and drag (rotation?)
-        player.body.acceleration.x = 0;
-        player.body.acceleration.y = 0;
+SPOON_APP.createInput = function() {
+    this.cursors = this.game.input.keyboard.createCursorKeys();
+    this.fireButton = this.game.input.keyboard.addKey(Phaser.KeyCode.SPACEBAR);
+}
 
-        if (cursors.left.isDown) {
-            player.body.angularVelocity = -180;
-        } else if (cursors.right.isDown) {
-            player.body.angularVelocity = 180;
-        } else {
-            player.body.angularVelocity = 0;
-        }
+SPOON_APP.createTexts = function() {
+    var labelStyle = { font: '24px Arial', fill: '#fff' };
+    this.score = 0;
+    this.scoreText = this.game.add.text(10, 10, this.SCORE_PREFIX + this.score, labelStyle);
 
-        var angle = player.angle * Math.PI / 180;
-        bullets.fireAngle = Phaser.ANGLE_UP + player.angle;
-        if (cursors.up.isDown) {
-            player.body.acceleration.x = 250 * Math.sin(angle);
-            player.body.acceleration.y = -250 * Math.cos(angle);
-        } else if (cursors.down.isDown) {
-            player.body.acceleration.x = -250 * Math.sin(angle);
-            player.body.acceleration.y = 250 * Math.cos(angle);
-        } else {
-            player.body.acceleration.y = 0;
-        }
-
-        if (fireButton.isDown) {
-            bullets.fire();
-        }
-
-        // TODO this is dumb
-        forks.forEach(function(fork) {
-            if (fork.body.y > 0 && !fork.body.collideWorldBounds) {
-                fork.body.collideWorldBounds = true;
-                fork.body.velocity.x = Math.random() * 400 - 200;
-            }
-        });
-
-        // TODO why doesn't game.physics.arcade.overlap(forks, bullets, bulletHitsEnemey, null, this) work??!?!
-        bullets.forEach(function(bullet) {
-            forks.forEach(function(fork) {
-                if (game.physics.arcade.intersects(fork.body, bullet.body)) {
-                    bulletHitsEnemey(bullet, fork);
-                }
-            });
-        });
-        game.physics.arcade.overlap(player, forks, playerHitsEnemy, null, this);
+    this.lives = 3;
+    this.livesText = this.game.add.text(this.game.world.width - 150, 10, this.LIVES_PREFIX, labelStyle);
+    for (var i = 0; i < this.lives; i++) {
+        this.livesText.text += "💛";
     }
 
-    // TODO based on CSS
-    game = new Phaser.Game(850, 600, Phaser.AUTO, gameDiv, {
-        preload: preload,
-        create: create,
-        update: update
+    this.enemyText = this.game.add.text(this.game.world.width - 150, this.game.world.height - 40, this.ENEMY_PREFIX + this.enemyLives, labelStyle);
+}
+
+SPOON_APP.create = function() {
+    this.enemyLives = 0;
+    this.starfield = this.game.add.tileSprite(0, 0, this.game.world.width, this.game.world.height, 'starfield');
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.createPlayer();
+    this.createBullets();
+    this.createForks();
+    this.createInput();
+    this.createTexts();
+}
+
+SPOON_APP.bulletHitsEnemey = function(bullet, enemy) {
+    bullet.kill();
+    enemy.destroy();
+    this.enemyLives--;
+    this.enemyText.text = this.ENEMY_PREFIX + this.enemyLives;
+
+    this.score += 100;
+    this.scoreText.text = this.SCORE_PREFIX + this.score;
+}
+
+SPOON_APP.playerHitsEnemy = function(player, enemy) {
+    enemy.destroy();
+    this.enemyLives--;
+    this.enemyText.text = this.ENEMY_PREFIX + this.enemyLives;
+    this.lives--;
+    this.score -= 200;
+    this.scoreText.text = this.SCORE_PREFIX + this.score;
+    if (this.lives > 0) {
+        // TODO "blink"
+        this.livesText.text = this.LIVES_PREFIX;
+        for (var i = 0; i < this.lives; i++) {
+            this.livesText.text += "💛";
+        }
+    } else {
+        // TODO
+        console.log('Game Over');
+    }
+}
+
+SPOON_APP.update = function() {
+
+
+    this.starfield.tilePosition.y += 2;
+    // TODO inertia and drag (rotation?)
+    this.player.body.acceleration.x = 0;
+    this.player.body.acceleration.y = 0;
+
+    if (this.cursors.left.isDown) {
+        this.player.body.angularVelocity = -180;
+    } else if (this.cursors.right.isDown) {
+        this.player.body.angularVelocity = 180;
+    } else {
+        this.player.body.angularVelocity = 0;
+    }
+
+    var angle = this.player.angle * Math.PI / 180;
+    this.bullets.fireAngle = Phaser.ANGLE_UP + this.player.angle;
+    if (this.cursors.up.isDown) {
+        this.player.body.acceleration.x = 250 * Math.sin(angle);
+        this.player.body.acceleration.y = -250 * Math.cos(angle);
+    } else if (this.cursors.down.isDown) {
+        this.player.body.acceleration.x = -250 * Math.sin(angle);
+        this.player.body.acceleration.y = 250 * Math.cos(angle);
+    } else {
+        this.player.body.acceleration.y = 0;
+    }
+
+    if (this.fireButton.isDown) {
+        this.bullets.fire();
+    }
+
+    // TODO this is dumb
+    this.forks.forEach(function(fork) {
+        if (fork.body.y > 0 && !fork.body.collideWorldBounds) {
+            fork.body.collideWorldBounds = true;
+            fork.body.velocity.x = Math.random() * 400 - 200;
+        }
     });
+
+    // TODO why doesn't game.physics.arcade.overlap(forks, bullets, bulletHitsEnemey, null, this) work??!?!
+    this.bullets.forEach(function(bullet) {
+        this.forks.forEach(function(fork) {
+            if (this.game.physics.arcade.intersects(fork.body, bullet.body)) {
+                this.bulletHitsEnemey(bullet, fork);
+            }
+        }, this);
+    }, this);
+    this.game.physics.arcade.overlap(this.player, this.forks, this.playerHitsEnemy, null, this);
+}
+
+SPOON_APP.main = function() {
+    // var player, cursors, bullets, fireButton, forks, score, scoreText, lives, livesText, starfield, enemyLives, enemyText;
+
+    var gameDiv = document.getElementById('game');
+    // TODO based on CSS
+    this.game = new Phaser.Game(850, 600, Phaser.AUTO, gameDiv, {
+        preload: function() {SPOON_APP.preload()},
+        create: function() {SPOON_APP.create()},
+        update: function() {SPOON_APP.update()}
+    });
+
 };
 
+window.onload = function() {
+    SPOON_APP.main();
+}
